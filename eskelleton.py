8eb79132c4beb73ab318d499c317f3d109573ff0
camilaconsentino <103431611+camilaconsentino@users.noreply.github.com>
2022-05-27 11:08:47 -0300

    x

--- eskelleton.py
+++ eskelleton.py
@@ -1,8 +1,13 @@
+import pygame
+pygame.init
 
 class HEAD(pygame.sprite.Sprite):
     def __init__(self, assets):
-        self.image = 
-        self.mask = 
+        
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets['roxocobra']
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = 
         self.rect.y = 
@@ -10,12 +15,18 @@ class HEAD(pygame.sprite.Sprite):
         self.speedy = 8
     
     def update(self):
-        self.rect.y += self.speedy
         if keys_pressed[pygame.K_LEFT]: #left
+            self.rect.x -= self.speedx
+        if keys_pressed[pygame.K_RIGHT]: #right
             self.rect.x += self.speedx
-        if keys_pressed[pygame.K_RIGHT] and cobra.x + VEL < WIDTH - (BORDER_DIAMETER + COBRA_WIDTH): #right
-            cobra.x += VEL
-        if keys_pressed[pygame.K_UP] and cobra.y - VEL > BORDER_DIAMETER: #up
-            cobra.y -= VEL
-        if keys_pressed[pygame.K_DOWN] and cobra.y + VEL < HEIGHT - (BORDER_DIAMETER + COBRA_HEIGHT): #down
-            cobra.y += VEL
+        if keys_pressed[pygame.K_UP]: #up
+            self.rect.y += self.speedy
+        if keys_pressed[pygame.K_DOWN]: #down
+            self.rect.y -= self.speedy
+
+class RAT(pygame.sprite.Sprite):
+    def __init__(self, assets):
+        
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets['rato']
--- main.py
+++ main.py
@@ -34,28 +34,6 @@ COBRA_WIDTH, COBRA_HEIGHT = 30, 30
 COBRA_IMG = pygame.image.load('cobra2.png').convert() #colocar nome da imagem do rato
 COBRA_PEQUENO_IMG = pygame.transform.scale(COBRA_IMG, (COBRA_WIDTH, COBRA_HEIGHT))
 
-#classes
-class HEAD(pygame.sprite.Sprite):
-    def __init__(self, assets):
-        self.image = 
-        self.mask = 
-        self.rect = self.image.get_rect()
-        self.rect.x = 
-        self.rect.y = 
-        self.speedx = 8
-        self.speedy = 8
-    
-    def update(self):
-        self.rect.y += self.speedy
-        if keys_pressed[pygame.K_LEFT]: #left
-            self.rect.x += self.speedx
-        if keys_pressed[pygame.K_RIGHT] and cobra.x + VEL < WIDTH - (BORDER_DIAMETER + COBRA_WIDTH): #right
-            cobra.x += VEL
-        if keys_pressed[pygame.K_UP] and cobra.y - VEL > BORDER_DIAMETER: #up
-            cobra.y -= VEL
-        if keys_pressed[pygame.K_DOWN] and cobra.y + VEL < HEIGHT - (BORDER_DIAMETER + COBRA_HEIGHT): #down
-            cobra.y += VEL
-
 def draw_window(cobra):
     WINDOW.fill(WHITE)
     pygame.draw.rect(WINDOW, BLACK, BORDER_LEFT)
